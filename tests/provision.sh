3ce1825e990a6bcd20e33db1817dd2d579246530
wick-ed <b.wick@techdivision.com>
2015-04-09 13:04:41 +0200

    bugfixes + packaged box check

--- tests/provision.sh
+++ tests/provision.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-# install ant
+# install ant and other dependencies
 yum -y install ant ant-contrib git;
+
+# download jmeter and make it usable
+wget ${jmeter.download.url}/${jmeter.package.name};
+tar -xvzf ./${jmeter.package.name} ${jmeter.vagrant.dir}
