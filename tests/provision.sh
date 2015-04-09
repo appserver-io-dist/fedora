234e32052ac7a205b6518504d2cdfdcef2365f82
wick-ed <b.wick@techdivision.com>
2015-04-09 14:20:29 +0200

    bugfix: doubled jmeter download url

--- tests/provision.sh
+++ tests/provision.sh
@@ -4,5 +4,5 @@
 yum -y install ant ant-contrib git;
 
 # download jmeter and make it usable
-wget ${jmeter.download.url}/${jmeter.package.name};
+wget ${jmeter.download.url};
 tar -xvzf ./${jmeter.package.name} ${jmeter.vagrant.dir}
